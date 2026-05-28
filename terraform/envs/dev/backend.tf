c7a04ab63b3923c2893f09103faba989c9d023e0
Juan Camilo Molina Mussen <juancamilomolina45@gmail.com>
2026-05-28 07:28:53 -0500

    feat(infra): add terraform env dev

---
+++ terraform/envs/dev/backend.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.6.0"
+  backend "s3" {
+    bucket                      = "circleguard-tfstate"
+    key                         = "envs/dev/terraform.tfstate"
+    region                      = "us-east-1"
+    dynamodb_table              = "circleguard-tflock"
+    endpoints = {
+      s3       = "http://localhost:4566"
+      dynamodb = "http://localhost:4566"
+    }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    force_path_style            = true
+    access_key                  = "test"
+    secret_key                  = "test"
+  }
+}
